9f7507180bc25bcce3ae6498805474678b4d7e6e
Dominik Macek <dominik.macek@fer.hr>
2018-12-03 15:49:21 +0100

    Side nav bar, navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatNativeDateModule } from '@angular/material';
 import { MyCustomMaterialModuleModule } from './my-custom-material-module/my-custom-material-module.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @NgModule({
   declarations: [
@@ -17,14 +22,16 @@ import { MyCustomMaterialModuleModule } from './my-custom-material-module/my-cus
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MyCustomMaterialModuleModule
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MyCustomMaterialModuleModule,
+    CommonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,20 +1,51 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 
+/** @title Responsive sidenav */
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css'],
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  fillerNav = Array.from({length: 10}, (_, i) => `Jako mnogo puno dugačak Link ${i + 1}`);
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  fillerContent = "main content lmao ";
 
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
+
+// import { Component } from '@angular/core';
+// import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+// import { Observable } from 'rxjs';
+// import { map } from 'rxjs/operators';
+
+// @Component({
+//   selector: 'app-main-nav',
+//   templateUrl: './main-nav.component.html',
+//   styleUrls: ['./main-nav.component.css'],
+// })
+// export class MainNavComponent {
+
+//   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+//     .pipe(
+//       map(result => result.matches)
+//     );
+
+//   constructor(private breakpointObserver: BreakpointObserver) {}
+
+// }
